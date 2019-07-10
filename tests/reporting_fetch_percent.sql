5ad5312c49fd04b41bd56b954dc75e1da36221dd
Ryan Lambert <ryan@rustprooflabs.com>
2019-07-09 18:25:14 -0600

    Describing test query.

--- tests/reporting_fetch_percent.sql
+++ tests/reporting_fetch_percent.sql
@@ -1,3 +1,17 @@
+/*
+	Test for the speed of queries using `FETCH FIRST N PERCENT ROWS ONLY`.
+
+	Wrapped query from ./reporting1.sql in CTE and moved aggregation to
+	outside the query.
+
+	Example invocation:
+
+		pgbench -c 2 -j 2 -T 600 -P 60 -s 10 \
+    		--define=fetch_percent=1 \
+    		-f reporting_fetch_percent.sql \
+    		bench_test
+
+*/
 WITH t AS (
  SELECT fk1, fk1data, v1, v2
     FROM pgbench_view
