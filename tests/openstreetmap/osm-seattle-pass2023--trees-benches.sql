6106f01befb4949ce95835c59044991b92be4a5f
Ryan Lambert <ryan@rustprooflabs.com>
2023-11-04 09:19:46 -0600

    Add bench test using Pass 2023 demo data

---
+++ tests/openstreetmap/osm-seattle-pass2023--trees-benches.sql
@@ -0,0 +1,19 @@
+EXPLAIN (ANALYZE, BUFFERS, SETTINGS, VERBOSE)
+WITH selected_place AS (
+SELECT osm_id
+    FROM osm_seattle.place_polygon
+    ORDER BY random()
+    LIMIT 1
+)
+SELECT p.osm_id, COUNT(DISTINCT a.osm_id) AS bench_count,
+        COUNT(n.osm_id )AS trees_near_bench_count
+    FROM selected_place sp
+    INNER JOIN osm_seattle.place_polygon p ON sp.osm_id = p.osm_id
+    LEFT JOIN osm_seattle.amenity_point a
+        ON ST_Contains(p.geom, a.geom)
+            AND a.osm_type = 'bench'
+    LEFT JOIN osm_seattle.natural_point n
+        ON ST_DWithin(a.geom, n.geom, 50)
+            AND n.osm_type = 'tree'
+    GROUP BY p.osm_id
+;
